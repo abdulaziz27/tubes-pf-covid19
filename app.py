a5aff835b69d7a83e76a10760ef53e34a7ec7bde
Abdul Aziz <doelziez212@gmail.com>
2023-02-06 13:08:48 +0700

    add filter and map function

--- app.py
+++ app.py
@@ -1,66 +1,64 @@
-from flask import Flask, render_template, url_for, request, jsonify
+from flask import Flask, render_template, request
 from flask_paginate import Pagination, get_page_args
+import requests
 
 app = Flask(__name__)
 
-import requests
-
-
 url = "https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/"
-
 endpoint_countries = 'countries'
 endpoint_world = 'world'
 
 headers = {
-	"X-RapidAPI-Key": "45f3db60cemsh852aa24317a3125p1aa9a1jsna01b5be57c21",
-	"X-RapidAPI-Host": "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com"
+    "X-RapidAPI-Key": "45f3db60cemsh852aa24317a3125p1aa9a1jsna01b5be57c21",
+    "X-RapidAPI-Host": "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com"
 }
 
-response = requests.request("GET", url + endpoint_countries, headers=headers)
-responseWorldData = requests.request("GET", url + endpoint_world, headers=headers)
+response_stats = requests.request("GET", url + endpoint_countries, headers=headers)
+response_world = requests.request("GET", url + endpoint_world, headers=headers)
 
-stats_data = response.json()
-world_data = responseWorldData.json()
+# Convert from json to python format 
+stats_data = response_stats.json()
+world_data = response_world.json()
 
-new_stats_list = []
-for item in stats_data:
-    stats = {
-        'country': item['Country'],
-        'total_cases': item['TotalCases'],
-        #'new_cases': item['NewCases'],
-        'total_deaths': item['TotalDeaths'],
-        #'new_death': item['NewDeaths'],
-        'total_recovered': item['TotalRecovered'],
-        #'new_recovered': item['NewRecovered'],
-        'active_cases': item['ActiveCases'],
-        'population': item['Population'],
-        'cfr': item['Case_Fatality_Rate'],
-        'recovery_presentage': item['Recovery_Proporation'],
-        'infection_risk': item['Infection_Risk'],
+# Filter and map stats_data into new list using map and filter
+new_stats_list = list(
+    map(
+        lambda item: {
+            'country': item['Country'],
+            'total_cases': item['TotalCases'],
+            'total_deaths': item['TotalDeaths'],
+            'total_recovered': item['TotalRecovered'],
+            'active_cases': item['ActiveCases'],
+            'population': item['Population'],
+            'cfr': item['Case_Fatality_Rate'],
+            'recovery_presentage': item['Recovery_Proporation'],
+            'infection_risk': item['Infection_Risk'],
+        },
+        filter(
+            lambda item: item['Country'] is not None,
+            stats_data
+        )
+    )
+)
 
-    }
-    new_stats_list.append(stats)
+# Sort new_stats_list using sorted
+sort_by_cfr = sorted(new_stats_list, key=lambda x: x['cfr'], reverse=True)
+sort_by_recovery_p = sorted(new_stats_list, key=lambda x: x['recovery_presentage'], reverse=True)
+sort_by_infection_risk = sorted(new_stats_list, key=lambda x: x['infection_risk'], reverse=True)
 
-sortByCFR = sorted(new_stats_list, key=lambda x: x['cfr'], reverse=True)
-sortByRecoveryP = sorted(new_stats_list, key=lambda x: x['recovery_presentage'], reverse=True)
-sortByInfectionRisk = sorted(new_stats_list, key=lambda x: x['infection_risk'], reverse=True)
-
-# Menggunakan sorted() untuk mengurutkan data berdasarkan total kematian terendah dan total recovered terbanyak
-sorted_data = sorted(new_stats_list, key=lambda x: (x["total_deaths"], x["total_recovered"]), reverse=True)
-
-new_world_data = []
-for item in world_data:
-    data = {
-        'total_cases': item['TotalCases'],
-        'new_cases': item['NewCases'],
-        'total_deaths': item['TotalDeaths'],
-        'new_deaths': item['NewDeaths'],
-        'total_recovered': item['TotalRecovered'],
-        'new_recovered': item['NewRecovered'],
-        'active_cases': item['ActiveCases'],
-    }
-    new_world_data.append(data)
-
+# Map world_data into new list using map
+new_world_data = list(
+    map(
+        lambda item: {
+            'total_cases': item['TotalCases'],
+            'new_cases': item['NewCases'],
+            'total_deaths': item['TotalDeaths'],
+            'total_recovered': item['TotalRecovered'],
+            'active_cases': item['ActiveCases'],
+        },
+        world_data
+    )
+)
 
 @app.route("/search", methods=["POST"])
 def search():
@@ -85,7 +83,7 @@ def statistics():
 
     def get_pagination(offset=0, per_page=10):
         return new_stats_list[offset: offset+per_page]
-
+        
     page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
     total = len(new_stats_list)
     pagination_data = get_pagination(offset=offset, per_page=per_page)
@@ -95,19 +93,15 @@ def statistics():
 
 @app.route("/infection-risk")
 def infection_risk():
-    return render_template('infection_risk.html', title = "Infection Risk", sortByInfectionRisk = sortByInfectionRisk)
+    return render_template('infection_risk.html', title = "Infection Risk", sort_by_infection_risk = sort_by_infection_risk)
 
 @app.route("/recovery-presentage")
 def recovery_presentage():
-    return render_template('recovery_presentage.html', title = "Recovery Presentage", sortByRecoveryP = sortByRecoveryP)
+    return render_template('recovery_presentage.html', title = "Recovery Presentage", sort_by_recovery_p = sort_by_recovery_p)
 
 @app.route("/cfr")
 def cfr():
-    return render_template('cfr.html', title = "CFR", sortByCFR = sortByCFR)
+    return render_template('cfr.html', title = "CFR", sort_by_cfr = sort_by_cfr)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-"""
-flask --app app --debug run
-"""
